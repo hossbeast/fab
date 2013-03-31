285d41838ed92400bd95ed3a0c94594e0364e8e2
Todd Freed <todd.freed@gmail.com>
2013-03-30 18:48:22 -0600

    applied gplv3 license

--- listwise/object.h
+++ listwise/object.h
@@ -1,3 +1,20 @@
+/* Copyright (c) 2012-2013 Todd Freed <todd.freed@gmail.com>
+
+   This file is part of listwise.
+   
+   listwise is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   listwise is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with listwise.  If not, see <http://www.gnu.org/licenses/>. */
+
 #ifndef _LISTWISE_OBJECT_H
 #define _LISTWISE_OBJECT_H
 
@@ -157,3 +174,8 @@ int lstack_obj_add(lstack* const restrict ls, const void* const restrict o, uint
 	__attribute__((nonnull));
 
 #endif
+
+
+
+
+
--- op/j/j.c
+++ op/j/j.c
@@ -1,3 +1,20 @@
+/* Copyright (c) 2012-2013 Todd Freed <todd.freed@gmail.com>
+
+   This file is part of listwise.
+   
+   listwise is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   listwise is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with listwise.  If not, see <http://www.gnu.org/licenses/>. */
+
 #include <stdlib.h>
 #include <string.h>
 #include <alloca.h>
@@ -132,3 +149,8 @@ int op_exec(operation* o, lstack* ls, int** ovec, int* ovec_len)
 
 	finally : coda;
 }
+
+
+
+
+
