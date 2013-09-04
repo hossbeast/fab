c667c7d3b62b4860fb193dd29e2cfc817f96b82a
Todd Freed <Todd Freed>
2013-09-04 16:53:41 -0600

    added vf/vp operators

---
+++ liblistwise/op/vp/vp.c
@@ -0,0 +1,75 @@
+/* Copyright (c) 2012-2013 Todd Freed <todd.freed@gmail.com>
+
+   This file is part of fab.
+   
+   fab is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   fab is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with fab.  If not, see <http://www.gnu.org/licenses/>. */
+
+#include <stdlib.h>
+#include <alloca.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+#include <dirent.h>
+
+#include <listwise/operator.h>
+#include <listwise/lstack.h>
+
+#include "liblistwise_control.h"
+
+/*
+
+vp operator - select preceeding
+
+NO ARGUMENTS
+
+OPERATION
+
+ 1. select items preceeding first selected item
+
+*/
+
+static int op_validate(operation* o);
+static int op_exec(operation*, lstack*, int**, int*);
+
+operator op_desc[] = {
+	{
+		  .s						= "vp"
+		, .optype				= LWOP_SELECTION_READ | LWOP_SELECTION_WRITE
+		, .op_validate	= op_validate
+		, .op_exec			= op_exec
+		, .desc					= "select preceeding"
+	}
+	, {}
+};
+
+int op_validate(operation* o)
+{
+	return 0;
+}
+
+int op_exec(operation* o, lstack* ls, int** ovec, int* ovec_len)
+{
+	if(ls->l)
+	{
+		int x;
+		LSTACK_ITERATE(ls, x, go);
+		if(!go)
+		{
+			fatal(lstack_last_set, ls, x);
+		}
+		LSTACK_ITEREND;
+	}
+
+	finally : coda;
+}
