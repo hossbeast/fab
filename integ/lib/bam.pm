75f4d02bd057af184b682a41234d88f6af98d8de
Todd Freed <todd.freed@gmail.com>
2021-02-27 17:10:10 -0800

    integ: integration tests
    Integration tests which invoke the devel binaries in scripted ways and
    assert on their behavior. Most of the tests right now are focused around
    changes / invalidation of special files, like modules, var files, and
    formulas.

---
+++ integ/lib/bam.pm
@@ -0,0 +1,153 @@
+# Copyright (c) 2017-2018 Todd Freed <todd.freed@gmail.com>
+#
+# This file is part of autominer.
+#
+# autominer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# autominer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+
+#package bam;
+
+use File::Basename;
+use Data::Dumper;
+use Time::HiRes qw|nanosleep|;
+
+use strict;
+use warnings;
+
+our $bam_fabpath;
+our $bam_testdir;
+our $bam_invalidate;
+our $bam_sweeper_period = 250 * 1000 * 1000;
+our $repodir;
+
+sub setup
+{
+  my $spec = shift;
+
+  $bam_invalidate = $ARGV[0] || die "missing required argument : invalidate";
+  $bam_fabpath = "$repodir/fab/fab/fab.devel.xapi";
+  $bam_testdir = tree_build(basename($0), $spec);
+
+  bam_bootstrap();
+
+  return $bam_testdir;
+}
+
+sub reconcile
+{
+  if($bam_invalidate eq "notify")
+  {
+    printf("sleep " . (($bam_sweeper_period * 4)/1000/1000/1000) . "\n");
+    nanosleep($bam_sweeper_period * 4);
+  }
+  bam_reconcile();
+  print Dumper bam_tree();
+}
+
+sub bam
+{
+  my @args = @_;
+  unshift @args, $bam_fabpath;
+
+  run({ cwd => $bam_testdir}, @args);
+}
+
+sub bam_bootstrap
+{
+  bam(
+      "bootstrap"
+    , "-K"
+    , "--system-config-path", "system-config"
+    , "--user-config-path", "user-config"
+    , "--project-config-path", "project-config"
+    , "--default-filesystem-invalidate", $bam_invalidate
+    , "--sweeper-period-nsec", $bam_sweeper_period
+  );
+
+  print Dumper bam_metadata();
+  print Dumper bam_tree();
+}
+
+sub bam_reconcile
+{
+  bam("--no-launch", "reconcile");
+}
+
+sub bam_stats
+{
+  my $name = shift;
+
+  # fetch stats
+  my %stats;
+  my @args = ("--no-launch", "stats");
+  push @args, $name if $name;
+  my $text = bam(@args);
+  for my $line (split(/\n/, $text))
+  {
+    chomp $line;
+    if($line =~ /^ *([-_a-z0-9]+) : ([0-9]+)$/) {
+      $stats{$1} = int($2);
+    } elsif ($line =~ /^ *([-_a-z0-9]+) : ([-a-zA-Z0-9\/]+)$/) {
+      $stats{$1} = $2;
+    }
+  }
+
+#for my $k (sort keys %stats)
+#{
+#  printf("%25s %s\n", $k, $stats{$k});
+#}
+
+  \%stats;
+}
+
+sub bam_metadata
+{
+  # fetch metadata
+  my %metadata;
+  my $text = bam("--no-launch", "metadata");
+  for my $line (split(/\n/, $text))
+  {
+    next unless $line =~ /^ *([-_a-z0-9]+) : (.*)/;
+    $metadata{$1} = $2;
+  }
+
+  \%metadata;
+}
+
+sub bam_describe
+{
+  my $text = bam("--no-launch", "describe", ".");
+  print("$text\n");
+}
+
+sub bam_tree
+{
+  my %metadata;
+  my $text = bam("--no-launch", "tree");
+
+  $text;
+}
+
+sub bam_modtree
+{
+  my %metadata;
+  my $text = bam("--no-launch", "tree", "-g", "modtree");
+
+  $text;
+}
+
+sub bam_build
+{
+  bam("--no-launch", "build");
+}
+
+1
---
+++ integ/lib/xlinux.pm
@@ -0,0 +1,134 @@
+# Copyright (c) 2017-2018 Todd Freed <todd.freed@gmail.com>
+#
+# This file is part of autominer.
+#
+# autominer is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# autominer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+
+#package xlinux;
+#
+#use strict;
+#use warnings;
+#
+#require Exporter;
+#our @ISA = qw|Exporter|;
+#our @EXPORT = qw|uxmkdir xfhopen uxfhopen uxopen xunlink uxunlink pr_set_pdeathsig awrite xread|;
+
+use Data::Dumper;
+use Errno ':POSIX';
+use Fcntl;
+use Fcntl 'SEEK_SET';
+use POSIX;
+
+# fatal mkdir but only fail when errno != EEXIST
+sub uxmkdir
+{
+  my $path = shift;
+  return if mkdir $path;
+  return if $!{EEXIST};
+  die "mkdir($path) : $!"
+}
+
+# fatal unlink
+sub xunlink
+{
+  my $path = shift;
+  return if unlink $path;
+  die "unlink($path) : $!";
+}
+
+# fatal unlink but only fail when errno != ENOENT
+sub uxunlink
+{
+  my $path = shift;
+  return if unlink $path;
+  return if $!{ENOENT};
+  die "unlink($path) : $!";
+}
+
+# fatal fhopen
+sub xfhopen
+{
+  my $path = shift;
+  my $fh;
+  my $r = open($fh, $path);
+  return $fh if $r;
+  die "open($path) : $!";
+}
+
+# fatal fhopen but only fail when errno not in { ENOENT, EEXIST }
+sub uxfhopen
+{
+  my $path = shift;
+  my $fh;
+  my $r = open($fh, $path);
+  return $fh if $r;
+  return if $!{ENOENT};
+  return if $!{EEXIST};
+  die "open($path) : $!";
+}
+
+# fatal open but only fail when errno not in { ENOENT, EEXIST }
+sub uxopen
+{
+  my ($path, $mode) = @_;
+
+  my $r = POSIX::open($path, $mode);
+  return $r if $r && $r >= 0;
+  return -1 if $!{ENOENT};
+  return -1 if $!{EEXIST};
+  die "open($path) : $!";
+}
+
+sub pr_set_pdeathsig
+{
+  my $sig = shift;
+
+  return unless $^O eq 'linux';
+
+  my(undef, undef, undef, undef, $machine) = POSIX::uname();
+  my $SYS_prctl = undef;
+  $SYS_prctl = 157 if $machine eq "x86_64";
+  $SYS_prctl = 172 if $machine =~ /^i[3456]86$/;
+
+  return unless $SYS_prctl;
+
+  my $PR_SET_PDEATHSIG = 1;   # at least it is on my machine
+
+  syscall($SYS_prctl, $PR_SET_PDEATHSIG, $sig) >= 0 or die "prctl failed : $!";
+}
+
+# call write and retry on EINTR
+sub awrite
+{
+  while(1)
+  {
+    # returns undef on failure
+    my $r = POSIX::write(@_);
+    last if defined $r;
+    die "write failed : $!" unless $!{EINTR};
+  }
+}
+
+sub xread
+{
+  my $r = POSIX::read(@_);
+  if(not defined $r)
+  {
+    die("read($_[0]) : $!") unless $!{EINTR};
+    $r = 0;
+  }
+
+  int($r)
+}
+
+1
