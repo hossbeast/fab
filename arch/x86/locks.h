80d0e5130c5cfde0982370834e1b29d5789af9cc
Todd Freed <todd.freed@gmail.com>
2021-02-27 16:42:22 -0800

    locks: depend on extern int32_t tid
    To use these locks, the application must define int32_t tid and
    initialize it sufficiently early.
    
    Signed-off-by: Todd Freed <todd.freed@gmail.com>

--- arch/x86/locks.h
+++ arch/x86/locks.h
@@ -15,36 +15,70 @@
    You should have received a copy of the GNU General Public License
    along with fab.  If not, see <http://www.gnu.org/licenses/>. */
 
-#ifndef _LOCKS
-#define _LOCKS
+#ifndef FABD_LOCKS
+#define FABD_LOCKS
 
-#include <stdint.h>
-#include <stdbool.h>
 #include <sys/syscall.h>
 #include <linux/futex.h>
+#include <unistd.h>
+
+#include "types.h"
 
 #include "barriers.h"
+#include "macros.h"
+#include "threads.h"
 
-static inline bool trylock_acquire(int32_t *lock, int32_t tid)
+struct trylock {
+  int32_t i32;
+};
+
+/* initializer for a lock in the held state */
+#define TRYLOCK_INIT_HELD { .i32 = INT32_MAX }
+
+static inline bool trylock_acquire(struct trylock * restrict lock)
 {
   int32_t zero = 0;
-  return __atomic_compare_exchange_n(lock, &zero, tid, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+
+  RUNTIME_ASSERT(tid);
+
+  /* the second argument to cas_n is read/write */
+  if(__atomic_compare_exchange_n(&lock->i32, &zero, tid, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)) {
+    return true;
+  }
+
+  return false;
 }
 
-static inline void trylock_release(int32_t *lock, int32_t tid)
+/* If the lock is held by the caller, releases the lock, otherwise does nothing */
+static inline void trylock_release(struct trylock * restrict lock)
 {
-  __atomic_compare_exchange_n(lock, &tid, 0, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+  int32_t ltid;
+
+  RUNTIME_ASSERT(tid);
+
+  ltid = tid;
+  __atomic_compare_exchange_n(&lock->i32, &ltid, 0, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
 }
 
-static inline void spinlock_acquire(int32_t *lock, int32_t tid)
+/* releases the lock without regard to the current state of the lock */
+static inline void trylock_reset(struct trylock * restrict lock)
+{
+  __atomic_store_n(&lock->i32, 0, __ATOMIC_SEQ_CST);
+}
+
+struct spinlock {
+  int32_t i32;
+};
+
+static inline void spinlock_acquire(struct spinlock * restrict lock)
 {
   int32_t zero;
 
-  while(1)
-  {
+  RUNTIME_ASSERT(tid);
+
+  while(1) {
     zero = 0;
-    if((__atomic_compare_exchange_n(lock, &zero, tid, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST)))
-    {
+    if((__atomic_compare_exchange_n(&lock->i32, &zero, tid, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST))) {
       break;
     }
     asm("PAUSE");
@@ -53,34 +87,54 @@ static inline void spinlock_acquire(int32_t *lock, int32_t tid)
   smp_mb();
 }
 
-static inline void spinlock_release(int32_t * lock, int32_t tid)
+static inline void spinlock_release(struct spinlock * restrict lock)
 {
-  smp_mb();
-  __atomic_compare_exchange_n(lock, &tid, 0, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
+  int32_t ltid;
+
+  RUNTIME_ASSERT(tid);
+
+  ltid = tid;
+  __atomic_compare_exchange_n(&lock->i32, &ltid, 0, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST);
 }
 
-static inline void futex_acquire(int32_t * futex, int32_t tid)
+struct futexlock {
+  int32_t i32;
+};
+
+static inline struct futexlock *futexlock_acquire(struct futexlock * restrict lock)
 {
   int32_t zero;
 
+  RUNTIME_ASSERT(tid);
+
+  /* blocks until the futex can transition from zero -> tid */
   while(1)
   {
     zero = 0;
-    if((__atomic_compare_exchange_n(futex, &zero, tid, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST))) {
+    if((__atomic_compare_exchange_n(&lock->i32, &zero, tid, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST))) {
       break;
     }
 
-    syscall(SYS_futex, futex, FUTEX_WAIT, tid, 0, 0, 0);
+    syscall(SYS_futex, &lock->i32, FUTEX_WAIT, tid, 0, 0, 0);
   }
   smp_mb();
+
+  return lock;
 }
 
-static inline void futex_release(int32_t * futex, int32_t tid)
+static inline void futexlock_release(struct futexlock * restrict lock)
 {
-  smp_mb();
-  if((__atomic_compare_exchange_n(futex, &tid, 0, false, __ATOMIC_SEQ_CST, __ATOMIC_SEQ_CST))) {
-    syscall(SYS_futex, futex, FUTEX_WAKE, 1, 0, 0, 0);
-  }
+  RUNTIME_ASSERT(tid);
+
+#if DEBUG || DEVEL
+  int32_t old;
+  old = __atomic_exchange_n(&lock->i32, 0, __ATOMIC_SEQ_CST);
+  RUNTIME_ASSERT(old == tid);
+#else
+  __atomic_store_n(&lock->i32, 0, __ATOMIC_SEQ_CST);
+#endif
+
+  syscall(SYS_futex, &lock->i32, FUTEX_WAKE, 1, 0, 0, 0);
 }
 
 #endif
